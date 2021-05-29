7560e6ba913dcace0b729fa0e4e5af7d461ec181
datbui <datbui6603@gmail.com>
2021-05-29 10:31:30 +0700

    create login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,17 +1,61 @@
-import React from 'react';
-import { useHistory } from 'react-router';
+import InputField from "components/customeField/InputField.jsx";
+import YLButton from "components/customeField/YLButton.jsx";
+import React from "react";
+import { useHistory } from "react-router";
+import * as Yup from "yup";
+import { FastField, Form, Formik } from "formik";
 
 function Login(props) {
-	const history = useHistory();
-	const login = () => {
-		localStorage.setItem('accessToken', true);
-		history.replace('/home');
-	};
-	return (
-		<div>
-			<button onClick={login}>Login</button>
-		</div>
-	);
+  const history = useHistory();
+  const login = (values) => {
+    console.log("Submit: ", values);
+    localStorage.setItem("accessToken", true);
+    history.replace("/home");
+  };
+
+  //constructor value for formik field
+  const initialValues = {
+    account: "",
+    password: "",
+  };
+  //check validate for formik field
+  const validationSchema = Yup.object().shape({
+    account: Yup.string().required("This field is required."),
+    password: Yup.string().required("This field is required."),
+  });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        login(values);
+      }}
+    >
+      {(formikProps) => {
+        const { values, errors, touched } = formikProps;
+        console.log({ values, errors, touched });
+        return (
+          <Form>
+            <FastField
+              name="account"
+              component={InputField}
+              label="Số điện thoại"
+              placeholder="Nhập số điện thoại"
+            ></FastField>
+            <FastField
+              name="password"
+			  type="password"
+              component={InputField}
+              label="Mật khẩu"
+              placeholder="Nhập mật khẩu"
+            ></FastField>
+            <YLButton type="submit" varian="primary" value="Login"></YLButton>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
 }
 
 export default Login;
