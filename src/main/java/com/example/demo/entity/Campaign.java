9ff71b0f2650a1331bc57eccd4eda9b078a2815a
thangnm <thangnmm@gmail.com>
2021-06-04 12:30:00 +0700

    draf

--- src/main/java/com/example/demo/entity/Campaign.java
+++ src/main/java/com/example/demo/entity/Campaign.java
@@ -1,38 +1,38 @@
-//package com.example.demo.entity;
-//
-//import com.sun.istack.Nullable;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//import java.util.Date;
-//
-//@Data
-//@Entity
-//@Table(name = "tbl_Campaign")
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Campaign {
-//
-//    @Id
-//    @Column(name = "campaignID")
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private long campaignID;
-//
-//    @Nullable
-//    @Column(name = "description")
-//    private String description;
-//
-//    @Nullable
-//    @Column(name = "startDate")
-//    private Date startDate;
-//
-//    @Nullable
-//    @Column(name = "endDate")
-//    private Date endDate;
-//
-//    @Nullable
-//    @Column(name = "banner")
-//    private String banner;
-//}
+package com.example.demo.entity;
+
+import com.sun.istack.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "tbl_Campaign")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Campaign {
+
+    @Id
+    @Column(name = "campaignID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long campaignID;
+
+    @Nullable
+    @Column(name = "description")
+    private String description;
+
+    @Nullable
+    @Column(name = "startDate")
+    private Date startDate;
+
+    @Nullable
+    @Column(name = "endDate")
+    private Date endDate;
+
+    @Nullable
+    @Column(name = "banner")
+    private String banner;
+}
