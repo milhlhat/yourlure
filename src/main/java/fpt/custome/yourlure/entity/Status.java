5cfb3bb2047c05a35cedf2d3d9e4e3e54d01f463
snpham1102 <snpham1102@gmail.com>
2021-06-08 17:15:56 +0700

    fix error code

---
+++ src/main/java/fpt/custome/yourlure/entity/Status.java
@@ -0,0 +1,32 @@
+package fpt.custome.yourlure.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.Nullable;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "tbl_Status")
+public class Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "statusID")
+    private Long statusID;
+
+    @Nullable
+    @Column(name = "statusName")
+    private String statusName;
+
+    @JsonIgnore
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "orderID", referencedColumnName = "statusID")
+    private Order order;
+
+}
