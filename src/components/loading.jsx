41d25eb7789d94fbd670065b6a93257f1b1824e2
datbui <datbui6603@gmail.com>
2021-06-11 16:35:01 +0700

    fix loading

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import 'assets/scss/scss-components/loading.scss';
+import React from "react";
+import "assets/scss/scss-components/loading.scss";
 function Loading() {
-	return (
-		<div class="spinner-border text-success" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-	);
+  return (
+    <div className="loading">
+      <div class="spinner-border text-success" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
--- src/components/user/ManagementAccount.jsx
+++ src/components/user/ManagementAccount.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import 'assets/scss/scss-components/user/management-account.scss'
 
 function ManagementAccount(props) {
   const account = {
@@ -9,34 +10,41 @@ function ManagementAccount(props) {
     userID: 0,
     userName: "string",
   };
+  const listTab = [
+    { name: "tab-account", component: <AccountTab /> },
+    { name: "tab-payment", component: <PaymentTab /> },
+  ];
+  const [tabOpen, setTapOpen] = useState(0);
+  const changeTab = (value) => {
+    setTapOpen(value);
+  };
   return (
-    <div>
+    <div className="management-account">
+      <div className="tab-switch">
         account work
-      <div className="tab-manage">
-            {/* {AccountTab} */}
+        <button onClick={() => changeTab(0)}>account</button>
+        <button onClick={() => changeTab(1)}>payment</button>
+      </div>
+      <div className="tab-show">
+        <div>{listTab[tabOpen].component}</div>
       </div>
     </div>
   );
 }
 
-
-// function AccountTab(props){
-
-//     return (
-//         <div>
-//             <span>account tab work</span>
-//         </div>
-//     );
-
-// }
-// function PaymentTab(props){
-
-//     return (
-//         <div>
-//             <span>payment tab work</span>
-//         </div>
-//     );
-
-// }
+function AccountTab(props) {
+  return (
+    <div>
+      <span>account tab work</span>
+    </div>
+  );
+}
+function PaymentTab(props) {
+  return (
+    <div>
+      <span>payment tab work</span>
+    </div>
+  );
+}
 
 export default ManagementAccount;
