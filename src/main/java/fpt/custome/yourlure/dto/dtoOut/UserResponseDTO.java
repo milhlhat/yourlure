a3c5ca8689292e5792c955709941c395fbfb46cf
thangnm <thangnmm@gmail.com>
2021-06-24 16:53:54 +0700

    edit user dto

--- src/main/java/fpt/custome/yourlure/dto/dtoOut/UserResponseDTO.java
+++ src/main/java/fpt/custome/yourlure/dto/dtoOut/UserResponseDTO.java
@@ -1,9 +1,12 @@
 package fpt.custome.yourlure.dto.dtoOut;
 
+import fpt.custome.yourlure.entity.Provider;
 import fpt.custome.yourlure.entity.Role;
+import fpt.custome.yourlure.entity.UserAddress;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.util.Collection;
 import java.util.List;
 @Data
 public class UserResponseDTO {
@@ -17,6 +20,15 @@ public class UserResponseDTO {
   @ApiModelProperty(position = 3)
   private String email;
   @ApiModelProperty(position = 4)
+  private Collection<UserAddress> userAddressCollection;
+  @ApiModelProperty(position = 5)
+  private Boolean gender;
+  @ApiModelProperty(position = 6)
+  private Integer maxCustomizable;
+  @ApiModelProperty(position = 7)
+  private Provider provider;
+
+  @ApiModelProperty(position = 7)
   List<Role> roles;
 
 
