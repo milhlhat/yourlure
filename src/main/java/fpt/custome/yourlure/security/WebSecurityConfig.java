05780daf0829f9b475b008dc42bafa50c432d8fa
thangnm <thangnmm@gmail.com>
2021-06-24 11:02:35 +0700

    fix webconfig

--- src/main/java/fpt/custome/yourlure/security/WebSecurityConfig.java
+++ src/main/java/fpt/custome/yourlure/security/WebSecurityConfig.java
@@ -27,9 +27,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-
-        // Disable CSRF (cross site request forgery)
-        http.cors();
         http.csrf().disable();
         // No session will be created or used by spring security
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -42,7 +39,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**/**").permitAll()
                 .antMatchers("/api/**").permitAll()
                 .antMatchers("/api/**/**").permitAll()
-//                .antMatchers("/swagger-ui.html").permitAll()
 
 
 //                .antMatchers("/api/product/model-download").permitAll()
