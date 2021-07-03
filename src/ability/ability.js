43c67bd44d54cb0759bc24807fb9c8280dec3701
datbui <datbui6603@gmail.com>
2021-07-03 23:41:43 +0700

    fix ability file

--- src/ability/ability.js
+++ src/ability/ability.js
@@ -4,22 +4,18 @@ import { ROLE_ADMIN, ROLE_STAFF, ROLE_CUSTOMER } from "constant/user-config";
 export default function defineAbilityFor(roles) {
   const { can, rules } = new AbilityBuilder(Ability);
   if (roles.length > 0) {
+    can("login", "website");
     if (roles.includes(ROLE_ADMIN)) {
-      can("login", "website");
       can("", "user");
     }
     if (roles.includes(ROLE_STAFF)) {
-      can("login", "website");
       can("edit", "product");
     }
     if (roles.includes(ROLE_CUSTOMER)) {
-      can("login", "website");
       can("manage", "my-info");
       can("buy", "product");
     }
   } else {
-    can("not-login", "website");
-
     can("buy", "product");
   }
 
