9b36d590b14c50e68be1938240b9f0de5f6df751
datbui <datbui6603@gmail.com>
2021-07-07 15:29:53 +0700

    dialog infomation

--- src/manager-page/component/manager-staff/ManagerStaff.jsx
+++ src/manager-page/component/manager-staff/ManagerStaff.jsx
@@ -4,22 +4,23 @@ import { filterConfig } from "constant/filter-setting";
 import React, { useEffect, useState } from "react";
 import Pagination from "react-js-pagination";
 import { useDispatch } from "react-redux";
-import { setIsBack } from 'redux/back-action/back-action';
+import { setIsBack } from "redux/back-action/back-action";
 import YLButton from "components/custom-field/YLButton";
-import './scss/manager-staff.scss';
+import "./scss/manager-staff.scss";
 import { useHistory } from "react-router-dom";
+import ConfirmPopup from "components/confirm-popup/ComfirmPopup";
 
 function ManagerStaff(props) {
-    const totalItem = 10;
-    const history = useHistory();
-  const [filterStaff,setFilterStaff]=useState({
+  const totalItem = 10;
+  const history = useHistory();
+  const [filterStaff, setFilterStaff] = useState({
     isAsc: true,
     keyword: "",
     limit: totalItem,
     page: 0,
     sortBy: "userId",
-    typeSearch: ""
-  })
+    typeSearch: "",
+  });
   const [staffList, setStaffList] = useState({
     data: [],
     isLoading: false,
@@ -28,32 +29,44 @@ function ManagerStaff(props) {
   const [activePage, setActivePage] = useState(1);
   function handlePageChange(newPage) {
     setActivePage(newPage);
-    setFilterStaff({...filterStaff,page:newPage-1})
+    setFilterStaff({ ...filterStaff, page: newPage - 1 });
   }
 
   const dispatch = useDispatch();
-  useEffect(()=>{
+  useEffect(() => {
     const action = setIsBack({
       canBack: false,
       path: "",
       label: "",
     });
     dispatch(action);
-  },[])
+  }, []);
 
-
-  const detectRole = (role)=>{
-    if(role){
-        if(role==="ROLE_ADMIN") return 'Quản lý'
-        if(role==="ROLE_STAFF") return 'Nhân viên'
-        if(role==="ROLE_CUSTOMER") return 'Khách hàng'
-        return 'chưa xác định'
-        
+  const detectRole = (role) => {
+    if (role) {
+      if (role === "ROLE_ADMIN") return "Quản lý";
+      if (role === "ROLE_STAFF") return "Nhân viên";
+      if (role === "ROLE_CUSTOMER") return "Khách hàng";
+      return "chưa xác định";
+    } else return "-";
+  };
+  const handleSwitchStatus = async (data) => {
+    try {
+      const response = await ManagerUserApi.switchStatus(data.userId);
+      if (response.data!=null &&!response.data) {
+        throw new Error();
+      }else {
+        fetchManagerStaff();
+      }
+    } catch (error) {
+      data.enabled
+        ? alert(`Chặn ${data.username ? data.username : data.phone} thất bại`)
+        : alert(
+            `Bỏ chặn ${data.username ? data.username : data.phone} thất bại`
+          );
+      console.log("fail to switch status user");
     }
-    else return '-';
-  }
-
-
+  };
   const fetchManagerStaff = async () => {
     setStaffList((prevState) => {
       return { ...prevState, isLoading: true };
@@ -69,27 +82,29 @@ function ManagerStaff(props) {
   useEffect(() => {
     fetchManagerStaff();
   }, [filterStaff]);
-  if(staffList.isLoading){
+  if (staffList.isLoading) {
     return <Loading />;
   } else
     return (
-        <>
-      <div className="staff-head-row d-flex justify-content-between">
-        <h3>Nhân viên</h3>
-        <div className="staff-add-new">
+      <>
+        <div className="staff-head-row d-flex justify-content-between">
+          <h3>Nhân viên</h3>
+          <div className="staff-add-new">
             <YLButton
               variant="primary"
               onClick={() => history.push("/manager/product/addnew")}
               value="Thêm"
-              to={"/manager/staff/addNew" }
+              to={"/manager/staff/addNew"}
             />
+          </div>
         </div>
-      </div>
-      <div className="manager-staff-show mt-3 bg-white bg-shadow">
+        <div className="manager-staff-show mt-3 bg-white bg-shadow">
           <span>tất cả Khách hàng</span>
           <hr />
           {/* <ManagerSort /> */}
-          {staffList?.data?.userDtoOutList?.length <= 0 && <p>Không có sản phẩm </p>}
+          {staffList?.data?.userDtoOutList?.length <= 0 && (
+            <p>Không có sản phẩm </p>
+          )}
           <table className="table">
             <tbody>
               <tr>
@@ -103,31 +118,47 @@ function ManagerStaff(props) {
                 <th></th>
               </tr>
               {staffList?.data?.userDtoOutList?.map((item, i) => (
-                  <tr key={'staff-'+i} className="hover-background">
-                    <td>
-                      {(activePage - 1) * totalItem +
-                        i +
-                        1}
-                    </td>
-                    <td>
-                      {item?.username?item?.username:'-'}
-                    </td>
-                    <td>
-                      {item.gender==null?'-':item.gender?'Nam':'Nữ'}
-                    </td>
-                    <td>{item.phone}</td>
-                    <td>{item.userEmail?item.userEmail:'-'}</td>
-                    <td>{item.enabled?'Hoạt động':'Không hoạt động'}</td>
-                    <td>
-                      {detectRole(item.roles[0])}
-                    </td>
-                    <td>
-                      {item.enabled&&<i className="far fa-user-slash"></i>}
-                      {!item.enabled&&<i className="far fa-user"></i>}
-                    
-                    </td>
-                  </tr>
-                ))}
+                <tr key={"staff-" + i} className="hover-background">
+                  <td>{(activePage - 1) * totalItem + i + 1}</td>
+                  <td>{item?.username ? item?.username : "-"}</td>
+                  <td>
+                    {item.gender == null ? "-" : item.gender ? "Nam" : "Nữ"}
+                  </td>
+                  <td>{item.phone}</td>
+                  <td>{item.userEmail ? item.userEmail : "-"}</td>
+                  <td>{item.enabled ? "Hoạt động" : "Không hoạt động"}</td>
+                  <td>{detectRole(item.roles[0])}</td>
+                  <td>
+                    {item.enabled ? (
+                      <ConfirmPopup
+                        variant="link"
+                        width="70px"
+                        height="25px"
+                        btnText={
+                          <i className="far fa-user-slash text-danger"></i>
+                        }
+                        title="Chặn"
+                        content={`Bạn chắc chắn muốn chặn ${
+                          item.username ? item.username : item.phone
+                        } ?`}
+                        onConfirm={() => handleSwitchStatus(item)}
+                      />
+                    ) : (
+                      <ConfirmPopup
+                        variant="link"
+                        width="70px"
+                        height="25px"
+                        title="Bỏ chặn"
+                        btnText={<i className="far fa-user text-success"></i>}
+                        content={`Bạn chắc chắn muốn bỏ chặn ${
+                          item.username ? item.username : item.phone
+                        } ?`}
+                        onConfirm={() => handleSwitchStatus(item)}
+                      />
+                    )}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
           <div className="m-auto p-4 d-flex justify-content-center">
@@ -144,7 +175,7 @@ function ManagerStaff(props) {
             )}
           </div>
         </div>
-    </>
+      </>
     );
 }
 
--- src/manager-page/component/managerUser/ManagerUser.jsx
+++ src/manager-page/component/managerUser/ManagerUser.jsx
@@ -4,22 +4,23 @@ import { filterConfig } from "constant/filter-setting";
 import React, { useEffect, useState } from "react";
 import Pagination from "react-js-pagination";
 import { useDispatch } from "react-redux";
-import { setIsBack } from 'redux/back-action/back-action';
-import './scss/manager-user.scss';
-import { Tooltip } from '@material-ui/core';
+import { setIsBack } from "redux/back-action/back-action";
+import "./scss/manager-user.scss";
+import { Tooltip } from "@material-ui/core";
+import ConfirmPopup from "components/confirm-popup/ComfirmPopup";
 
 ManagerUser.propTypes = {};
 
 function ManagerUser(props) {
   const totalItem = 10;
-  const [filterUser,setFilterUser]=useState({
+  const [filterUser, setFilterUser] = useState({
     isAsc: true,
     keyword: "",
     limit: totalItem,
     page: 0,
     sortBy: "userId",
-    typeSearch: ""
-  })
+    typeSearch: "",
+  });
   const [userList, setUserList] = useState({
     data: [],
     isLoading: false,
@@ -28,19 +29,36 @@ function ManagerUser(props) {
   const [activePage, setActivePage] = useState(1);
   function handlePageChange(newPage) {
     setActivePage(newPage);
-    setFilterUser({...filterUser,page:newPage-1})
+    setFilterUser({ ...filterUser, page: newPage - 1 });
   }
 
   const dispatch = useDispatch();
-  useEffect(()=>{
+  useEffect(() => {
     const action = setIsBack({
       canBack: true,
       path: "/manager/product",
       label: "Product",
     });
     dispatch(action);
-  },[])
+  }, []);
 
+  const handleSwitchStatus = async (data) => {
+    try {
+      const response = await ManagerUserApi.switchStatus(data.userId);
+      if (response.data!=null &&!response.data) {
+        throw new Error();
+      }else {
+        fetchManagerUser();
+      }
+    } catch (error) {
+      data.enabled
+        ? alert(`Chặn ${data.username ? data.username : data.phone} thất bại`)
+        : alert(
+            `Bỏ chặn ${data.username ? data.username : data.phone} thất bại`
+          );
+      console.log("fail to switch status user");
+    }
+  };
 
   const fetchManagerUser = async () => {
     setUserList((prevState) => {
@@ -57,21 +75,22 @@ function ManagerUser(props) {
   useEffect(() => {
     fetchManagerUser();
   }, [filterUser]);
-  if(userList.isLoading){
+  if (userList.isLoading) {
     return <Loading />;
   } else
-  return (
-    <>
-      <div className="user-head-row">
-        <h3>Khách hàng</h3>
-        <div className="product-add-new">
+    return (
+      <>
+        <div className="user-head-row">
+          <h3>Khách hàng</h3>
+          <div className="product-add-new"></div>
         </div>
-      </div>
-      <div className="manager-user-show mt-3 bg-white bg-shadow">
+        <div className="manager-user-show mt-3 bg-white bg-shadow">
           <span>Tất cả Khách hàng</span>
           <hr />
           {/* <ManagerSort /> */}
-          {userList?.data?.userDtoOutList?.length <= 0 && <p>Không có sản phẩm </p>}
+          {userList?.data?.userDtoOutList?.length <= 0 && (
+            <p>Không có sản phẩm </p>
+          )}
           <table className="table">
             <tbody>
               <tr>
@@ -85,38 +104,45 @@ function ManagerUser(props) {
                 <th></th>
               </tr>
               {userList?.data?.userDtoOutList?.map((item, i) => (
-                  <tr key={'manager-user-'+i} className="hover-background">
-                    <td>
-                      {(activePage - 1) * totalItem +
-                        i +
-                        1}
-                    </td>
-                    <td>
-                      {item?.username?item?.username:'-'}
-                    </td>
-                    <td>
-                      {item.gender==null?'-':item.gender?'Nam':'Nữ'}
-                    </td>
-                    <td>{item.phone}</td>
-                    <td>{item.userEmail?item.userEmail:'-'}</td>
-                    <td>{item.enabled?'Hoạt động':'Không hoạt động'}</td>
-                    <td>
-                      {item.numberOfOrder}
-                    </td>
-                    <td>
-                    
-                    {item.enabled&&<Tooltip title="Chặn"> 
-                      
-                      <i className="far fa-user-slash pointer" onClick={()=>console.log("đã chặn ",item.userId)}></i>
-                      </Tooltip>}
-                    {!item.enabled&&<Tooltip title="Bỏ chặn">
-                      
-                    <i className="far fa-user pointer"></i>
-                      </Tooltip>}
-                    
-                    </td>
-                  </tr>
-                ))}
+                <tr key={"manager-user-" + i} className="hover-background">
+                  <td>{(activePage - 1) * totalItem + i + 1}</td>
+                  <td>{item?.username ? item?.username : "-"}</td>
+                  <td>
+                    {item.gender == null ? "-" : item.gender ? "Nam" : "Nữ"}
+                  </td>
+                  <td>{item.phone}</td>
+                  <td>{item.userEmail ? item.userEmail : "-"}</td>
+                  <td>{item.enabled ? "Hoạt động" : "Không hoạt động"}</td>
+                  <td>{item.numberOfOrder}</td>
+                  <td>
+                    {item.enabled ? (
+                      <Tooltip title="Chặn">
+                        <ConfirmPopup
+                          variant="link"
+                          width="70px"
+                          height="25px"
+                          btnText={
+                            <i className="far fa-user-slash text-danger"></i>
+                          }
+                          content={`Bạn chắc chắn muốn chặn ${item.username?item.username:item.phone} ?`}
+                          onConfirm={() => handleSwitchStatus(item)}
+                        />
+                      </Tooltip>
+                    ) : (
+                      <Tooltip title="Bỏ chặn">
+                        <ConfirmPopup
+                          variant="link"
+                          width="70px"
+                          height="25px"
+                          btnText={<i className="far fa-user text-success"></i>}
+                          content={`Bạn chắc chắn muốn bỏ chặn ${item.username?item.username:item.phone} ?`}
+                          onConfirm={() => handleSwitchStatus(item)}
+                        />
+                      </Tooltip>
+                    )}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
           <div className="m-auto p-4 d-flex justify-content-center">
@@ -133,8 +159,8 @@ function ManagerUser(props) {
             )}
           </div>
         </div>
-    </>
-  );
+      </>
+    );
 }
 
 export default ManagerUser;
