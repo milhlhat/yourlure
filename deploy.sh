623d53bc4a18a82e0424974a64795ea18b95225c
datbui <datbui6603@gmail.com>
2021-07-14 00:13:23 +0700

    cart api variant

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #pull new code
 git pull
+
+#update node module
+npm i
+
 # Build reactjs app with production mode
 npm run build
 
 # Run static file at port 5000
 npm install -g serve
-serve -s build
+nohup serve -s build -l -5620
 
