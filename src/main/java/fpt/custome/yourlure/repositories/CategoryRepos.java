981abd5178c6c23b865c85f6739a1ef626545d90
zeno4499 <snpham1102@gmail.com>
2021-07-16 09:40:28 +0700

    fix list best seller product

--- src/main/java/fpt/custome/yourlure/repositories/CategoryRepos.java
+++ src/main/java/fpt/custome/yourlure/repositories/CategoryRepos.java
@@ -22,13 +22,13 @@ public interface CategoryRepos extends JpaRepository<Category, Long> {
             " ORDER BY sum_quantity DESC ", nativeQuery = true)
     List<Category> getBestSellerWithCategory();
 
-    @Query(value = "SELECT tbl_category.*, SUM(tbl_order_line.quantity) AS sum_quantity\n" +
-            "FROM tbl_category, tbl_products, tbl_variants, tbl_order_line\n" +
-            "WHERE tbl_category.category_id = tbl_products.category_id\n" +
-            "AND tbl_products.product_id = tbl_variants.product_id\n" +
-            "AND tbl_variants.variant_id = tbl_order_line.variant_id\n" +
-            "GROUP BY tbl_category.category_name,tbl_category.category_id\n" +
-            "ORDER BY sum_quantity DESC ", nativeQuery = true)
+    @Query(value = "SELECT ct.*, SUM(ol.quantity) AS sum_quantity\n" +
+            "FROM tbl_category ct\n" +
+            "left join tbl_products pr on ct.category_id = pr.category_id\n" +
+            "left join tbl_variants v on pr.product_id = v.product_id\n" +
+            "left  join tbl_order_line ol on v.variant_id = ol.variant_id\n" +
+            "GROUP BY ct.category_name,ct.category_id\n" +
+            "ORDER BY sum_quantity DESC,ct.category_id ASC ", nativeQuery = true)
     List<Category> getBestSellerCategory();
 
     List<Category> findByCategoryNameContainsIgnoreCase(String keyword, Pageable pageable);
