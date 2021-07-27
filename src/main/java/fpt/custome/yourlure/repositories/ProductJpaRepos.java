24c9f055b2f072063187b53031fec069bdd97f66
zeno4499 <snpham1102@gmail.com>
2021-07-27 07:46:55 +0700

    fix query best seller

--- src/main/java/fpt/custome/yourlure/repositories/ProductJpaRepos.java
+++ src/main/java/fpt/custome/yourlure/repositories/ProductJpaRepos.java
@@ -12,18 +12,20 @@ import java.util.List;
 public interface ProductJpaRepos extends JpaRepository<Product, Long> {
 
     @Query(value = "SELECT\n" +
-            "        pr.*,\n" +
-            "        COALESCE(SUM(ol.quantity),0) AS sumQuantity  \n" +
-            "    FROM\n" +
-            "        tbl_products pr\n" +
-            "\t\t\t\t  LEFT JOIN tbl_variants v ON pr.product_id = v.product_id \n" +
-            "    LEFT JOIN tbl_order_line ol ON  ol.variant_id = v.variant_id \n" +
-            "  where pr.visible_in_storefront = true " +
-            "    GROUP BY\n" +
-            "        pr.product_name,\n" +
-            "        pr.product_id \n" +
-            "    ORDER BY\n" +
-            "        sumQuantity,product_id DESC LIMIT 10\n", nativeQuery = true)
+            "pr.*,\n" +
+            "COALESCE ( SUM ( ol.quantity ), 0 ) AS sumQuantity \n" +
+            "FROM\n" +
+            "tbl_products pr\n" +
+            "LEFT JOIN tbl_variants v ON pr.product_id = v.product_id\n" +
+            "LEFT JOIN tbl_order_line ol ON ol.variant_id = v.variant_id \n" +
+            "WHERE\n" +
+            "pr.visible_in_storefront = TRUE \n" +
+            "GROUP BY\n" +
+            "pr.product_name,\n" +
+            "pr.product_id \n" +
+            "ORDER BY\n" +
+            "sumQuantity DESC, product_id DESC\n" +
+            "LIMIT 10", nativeQuery = true)
     List<Product> bestSellerProduct();
 
     @Query(value = "SELECT\n" +
@@ -38,7 +40,7 @@ public interface ProductJpaRepos extends JpaRepository<Product, Long> {
             "pr.product_name,\n" +
             "pr.product_id \n" +
             "ORDER BY\n" +
-            "sumQuantity,product_id DESC  ", nativeQuery = true)
+            "sumQuantity DESC, product_id DESC\n", nativeQuery = true)
     List<Product> bestSellerProductByCategory(Long id);
 
 
