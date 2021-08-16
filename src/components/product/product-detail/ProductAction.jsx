9835cde7124aabf04a19ed886563a275657eb8a3
NhatNM <nhatnmofficial@gmail.com>
2021-08-16 18:44:17 +0700

    fix messages add to cart

--- src/components/product/product-detail/ProductAction.jsx
+++ src/components/product/product-detail/ProductAction.jsx
@@ -100,7 +100,11 @@ function ProductAction(props) {
       (product.maxWeight && data.weight > product.maxWeight) ||
       (product.minWeight && data.weight < product.minWeight)
     ) {
-      toast.warning("Độ nặng không phù hợp, vui lòng chỉnh lại độ nặng");
+      toast.warning(
+        `Vui lòng chọn độ nặng trong khoảng ${Number(
+          product.minWeight
+        )} - ${Number(product.maxWeight)}`
+      );
       setValue("weight", product.defaultWeight);
       setAddCart(false);
     } else {
