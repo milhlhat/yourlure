6c9ad2dcf09484c246eeb418ceae875232e9663e
zeno4499 <snpham1102@gmail.com>
2021-08-19 22:48:04 +0700

    fix bug #64

--- src/main/java/fpt/custome/yourlure/repositories/UserRepos.java
+++ src/main/java/fpt/custome/yourlure/repositories/UserRepos.java
@@ -21,16 +21,20 @@ public interface UserRepos extends JpaRepository<User, Long> {
             "and concat(lower(unaccent(u.username)),\n " +
             "lower(unaccent(u.phone)),o.order_id)\n " +
             "like lower(unaccent(?1))\n" +
-            "GROUP BY u.user_id" , nativeQuery = true)
-    Page<User> findAllUser(String keyword,Pageable pageable);
+            "GROUP BY u.user_id", nativeQuery = true)
+    Page<User> findAllUser(String keyword, Pageable pageable);
 
-    @Query(value = "SELECT distinct u.* \n" +
-            "from tbl_users u,user_roles ur\n" +
-            "WHERE u.user_id = ur.user_user_id\n" +
-            "and ur.roles in (1,0) " +
-            "and concat(lower(unaccent(u.username))," +
-            "lower(unaccent(u.phone))) like lower(unaccent(?1))", nativeQuery = true)
-    Page<User> findAllStaff(String keyword,Pageable pageable);
+    @Query(value = " SELECT u.*, COUNT(o.order_id) AS number_of_order \n" +
+            "from tbl_users u\n" +
+            "LEFT JOIN user_roles ur on u.user_id = ur.user_user_id\n" +
+            "LEFT JOIN tbl_orders o on u.user_id = o.user_id\n " +
+            "where \n " +
+            "ur.roles in (1,0)\n " +
+            "and concat(lower(unaccent(u.username)),\n " +
+            "lower(unaccent(u.phone)),o.order_id)\n " +
+            "like lower(unaccent(?1))\n" +
+            "GROUP BY u.user_id", nativeQuery = true)
+    Page<User> findAllStaff(String keyword, Pageable pageable);
 
     boolean existsByPhone(String phone);
 
