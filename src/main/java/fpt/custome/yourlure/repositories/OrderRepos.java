134b3207b7828be4476ade6aa9e2786a42c2fe83
zeno4499 <snpham1102@gmail.com>
2021-08-30 12:28:11 +0700

    fix bug lần cuối

--- src/main/java/fpt/custome/yourlure/repositories/OrderRepos.java
+++ src/main/java/fpt/custome/yourlure/repositories/OrderRepos.java
@@ -12,9 +12,9 @@ public interface OrderRepos extends JpaRepository<Order, Long> {
 
     List<Order> findAllByUserUserId(Long id);
 
-    @Query(value = "select * from tbl_orders\n" +
-            "where Concat(upper(unaccent(tbl_orders.receiver_name)),\n" +
-            "upper(unaccent(tbl_orders.order_code))) like upper(unaccent(?1))",nativeQuery = true)
+    @Query(value = "select ord.* from tbl_orders ord\n" +
+            " where Concat(upper(unaccent(ord.receiver_name)),\n" +
+            "upper(unaccent(ord.order_code))) like upper(unaccent(?1))",nativeQuery = true)
     Page<Order> findAllOrder(String keyword, Pageable pageable);
 
     Page<Order> findAllByPhoneContainsIgnoreCase(String keyword, Pageable pageable);
